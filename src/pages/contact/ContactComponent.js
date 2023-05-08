c13fcf472bed87ae65ddc133eadb650e8385fe87
Adrian <89922200+Adrian781ST@users.noreply.github.com>
2023-05-08 10:17:12 -0500

    Third

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
-import BlogsImg from "./BlogsImg";
 import { Fade } from "react-reveal";
 import "./ContactComponent.css";
 import { greeting, contactPageData } from "../../portfolio.js";
